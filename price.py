7db28149329f1090dc6df4853b636a7b25dcb740
Sergey Osotov <sindik63@gmail.com>
2018-11-25 14:29:30 +0700

    мы добавили строку вывода

--- price.py
+++ price.py
@@ -2,4 +2,5 @@ def format_price(price):
 	round(price)
 	return('цена: '+ str(price) + ' руб.')
 display_price=format_price(56.24)
+
 print(display_price)
--- user_info.py
+++ user_info.py
@@ -1,2 +1,3 @@
 user_info={'first_name':'Sergey','last_name':'Osotov'}
 print(user_info['first_name'])
+print(user_info['last_name'])
